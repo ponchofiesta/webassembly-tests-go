9e4d828838df63c2dfece9f8398ab1aa9d3e20ff
Michael Richter <m.richter@tu-berlin.de>
2019-04-09 16:32:42 +0200

    sort started

---
+++ src/tests/sort.go
@@ -0,0 +1,26 @@
+package tests
+
+import "sort"
+
+type Person struct {
+	Id   int
+	Name string
+}
+
+type ByName []Person
+
+func (a ByName) Len() int {
+	return len(a)
+}
+
+func (a ByName) Less(i, j int) bool {
+	return a[i].Name < a[j].Name
+}
+
+func (a ByName) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func sorter() {
+	return sort.Sort(ByName(data))
+}
